ed7e8a21f7c3cb39a4eb8f4d693b5a50ffa95257
liza-wood <belwood@ucdavis.edu>
2023-08-25 11:34:41 +0100

    before changing cslref tex

--- prep_papers.R
+++ prep_papers.R
@@ -2,3 +2,4 @@ source('~/Documents/Davis/R-Projects/organicseed_adoption/ch1-manuscript/01_down
 source('~/Documents/Davis/R-Projects/osisn_spatial/ch2-manuscript/01_download_chapter.R')
 source('~/Documents/Davis/R-Projects/osisn_processes/ch3-manuscript/01_download_chapter.R')
 
+
